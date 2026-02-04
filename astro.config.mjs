29894ae995de274563192fe42f671eecea9c4cf6
larry1sf <larryycel@gmail.com>
2026-02-04 15:04:26 -0500

    using vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "static",
+  adapter: vercel(),
   image: {
     domains: ["https://rickandmortyapi.com/api/character/avatar/"],
     remotePatterns: [{ protocol: "https", hostname: "rickandmortyapi.com" }],
   },
   vite: {
     plugins: [tailwindcss()],
-    server: {
-      watch: {
-        ignored: ["**/.cache/**"],
-      },
-    },
   },
 });
